a80b622bd4bbd827885a798d73d269b2ee0d303c
TiegoRama <ramatiego@gmail.com>
2025-03-17 17:10:08 +0100

    lundi

--- src/main/kotlin/org/ldv/savonapi/controller/IngredientController.kt
+++ src/main/kotlin/org/ldv/savonapi/controller/IngredientController.kt
@@ -1,28 +1,72 @@
+// Fichier : IngredientController.kt
 package org.ldv.savonapi.controller
 
 import org.ldv.savonapi.model.dao.IngredientDAO
 import org.ldv.savonapi.model.entity.Ingredient
-import org.ldv.savonapi.model.entity.Recette
 import org.springframework.http.HttpStatus
 import org.springframework.http.ResponseEntity
-import org.springframework.web.bind.annotation.CrossOrigin
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.RequestMapping
-import org.springframework.web.bind.annotation.RestController
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @CrossOrigin
 @RequestMapping("/api-savon/v1/ingredient")
-class IngredientController (val ingredientDAO: IngredientDAO){
+class IngredientController(val ingredientDAO: IngredientDAO) {
 
     @GetMapping
     fun index(): List<Ingredient> {
-        return this.ingredientDAO.findAll()
+        return ingredientDAO.findAll()
     }
+
+    @GetMapping("/{id}")
+    fun show(@PathVariable id: Long): ResponseEntity<Ingredient> {
+        return ingredientDAO.findById(id)
+            .map { ingredient -> ResponseEntity.ok(ingredient) }
+            .orElse(ResponseEntity.notFound().build())
+    }
+
     @PostMapping
-    fun createExemple(@RequestBody ingredient: Ingredient): ResponseEntity<Ingredient> {
+    fun store(@RequestBody ingredient: Ingredient): ResponseEntity<Ingredient> {
         val savedIngredient = ingredientDAO.save(ingredient)
         return ResponseEntity.status(HttpStatus.CREATED).body(savedIngredient)
+    }
+
+    @PutMapping("/{id}")
+    fun update(
+        @PathVariable id: Long,
+        @RequestBody updatedIngredient: Ingredient
+    ): ResponseEntity<Ingredient> {
+        return ingredientDAO.findById(id).map { existingIngredient ->
+            existingIngredient.apply {
+                nom = updatedIngredient.nom
+                iode = updatedIngredient.iode
+                ins = updatedIngredient.ins
+                sapo = updatedIngredient.sapo
+                volMousse = updatedIngredient.volMousse
+                tenueMousse = updatedIngredient.tenueMousse
+                douceur = updatedIngredient.douceur
+                lavant = updatedIngredient.lavant
+                durete = updatedIngredient.durete
+                solubilite = updatedIngredient.solubilite
+                sechage = updatedIngredient.sechage
+                estCorpsGras = updatedIngredient.estCorpsGras
+            }
+            ResponseEntity.ok(ingredientDAO.save(existingIngredient))
+        }.orElse(ResponseEntity.notFound().build())
+    }
+
+    @DeleteMapping("/{id}")
+    fun delete(@PathVariable id: Long): ResponseEntity<Void> {
+        return if (ingredientDAO.existsById(id)) {
+            ingredientDAO.deleteById(id)
+            ResponseEntity.noContent().build()
+        } else {
+            ResponseEntity.notFound().build()
+        }
+    }
+
+    @DeleteMapping("/all")
+    fun deleteAll(): ResponseEntity<Void> {
+        ingredientDAO.deleteAll()
+        return ResponseEntity.noContent().build()
     }
 }
